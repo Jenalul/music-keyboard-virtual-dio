f0f0689edc643a9002dc03a847d8ae86180bc413
JenaCarry <jeandaisu@gmail.com>
2025-05-28 12:34:04 -0300

    modificando

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,40 +1,44 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
-const volumeSlider = document.querySelector(".volume-slider input");
-const keysCheck = document.querySelector(".keys-check input");
-
-let mapedKeys = [];
-let audio = new Audio("src/tunes/a.wav");
+const volumeSlider = document.querySelector("#volume");
+const keysCheck = document.querySelector("#checked");
+const mapedKeys = [];
+let audio = new Audio();
+const pressedKeys = new Set();
 
 const playTune = (key) => {
-  audio.src = `src/tunes/${key}.wav`;
-  audio.play();
-
-  const clickedKey = document.querySelector(`[data-key="${key}"]`);
-  clickedKey.classList.add("active");
-  setTimeout(() => {
-    clickedKey.classList.remove("active");
-  }, 150);
+    audio.src = `./src/tunes/${key}.wav`;
+    audio.play();
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
 };
 
 pianoKeys.forEach((key) => {
-  key.addEventListener("click", () => playTune(key.dataset.key));
-  mapedKeys.push(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-  if (mapedKeys.includes(e.key)) {
-    playTune(e.key);
-  }
+    if (mapedKeys.includes(e.key) && !pressedKeys.has(e.key)) {
+        pressedKeys.add(e.key);
+        playTune(e.key);
+    }
+});
+
+document.addEventListener("keyup", (e) => {
+    pressedKeys.delete(e.key);
 });
 
 const handleVolume = (e) => {
-  audio.volume = e.target.value;
-};
-
-const showHideKeys = () => {
-  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+    audio.volume = e.target.value;
 };
 
 volumeSlider.addEventListener("input", handleVolume);
+
+const showHideKeys = () => {
+    pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
 
 keysCheck.addEventListener("click", showHideKeys);
